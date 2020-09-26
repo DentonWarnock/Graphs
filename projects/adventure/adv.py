686a28960f582dbcf8bb3e8be13bdadb3ea71a86
DentonWarnock <dentonwarnock@gmail.com>
2020-09-25 19:19:32 -0700

    adv.py tests passing in 992 moves.

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -11,10 +11,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -28,7 +28,7 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-opposite_directions = {
+opposite_direction = {
     'n': 's',
     's': 'n',
     'e': 'w',
@@ -40,48 +40,44 @@ def traverse_graph():
     visited = set()    
     # add starting room to visited
     visited.add(player.current_room)
-     
+    
+    path = []
+    stack = []
     
     # while we have not visited each room
-    while len(visited) < len(world.rooms):        
-        current_room = player.current_room        
-        
-        # make list of valid exits of non visted rooms
+    while len(visited) < len(room_graph):        
+        # make list of all valid exits of non visited rooms ['n', 's']
         valid_exits = []        
-        for direction in current_room.get_exits():            
-            if current_room.get_room_in_direction(direction) not in visited: 
+        for direction in player.current_room.get_exits():            
+            if player.current_room.get_room_in_direction(direction) not in visited: 
                 valid_exits.append(direction)
                 
-        # randomize exits - creates DFS        
-        random.shuffle(valid_exits)
+        # randomize the exits - making this a DFS - however results improved without (992 vs 996)
+        # random.shuffle(valid_exits)
         
         # if we have a valid exit/direction to travel
         if len(valid_exits) > 0:        
-            direction = valid_exits[0]  
-                      
-            # travel in the random direction
-            player.travel(direction)         
+            # travel in the first direction
+            direction = valid_exits[0]                     
+            player.travel(direction)
+            # add new current room to visited
+            visited.add(player.current_room)            
+            # add direction to path and stack
+            path.append(direction)  
+            stack.append(direction)                     
             
-            # store new room we are now in
-            new_room = player.current_room            
-                    
-            # add new room to visited
-            visited.add(new_room)
-            
-            # add direction to traversal_path
-            traversal_path.append(direction)   
-                    
-            
-        # else we have been here before or we're at an end
+        # else we need to reverse directions from the stack until we find a new exit
         else:
-            # travel in the opposite direction from last move
-            player.travel(opposite_directions[traversal_path[-1]])
-            
-            # add direction to traversal_path
-            traversal_path.append(opposite_directions[traversal_path[-1]])
-                
+            # get last direction from stack
+            last_valid_direction = stack.pop()
+            # travel in the opposite direction
+            player.travel(opposite_direction[last_valid_direction])            
+            # add direction to path
+            path.append(opposite_direction[last_valid_direction])
+        
+        # continue until all rooms are visited
     
-    return traversal_path    
+    return path    
         
     
 traversal_path = traverse_graph()
